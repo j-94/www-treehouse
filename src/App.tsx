38cf7096e0f2142d4f7569d731a1d6f96b02757e
kn1026 <116099862+kn1026@users.noreply.github.com>
2024-12-15 02:11:31 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React - Theserenis</h1>
+      <div className="card">
+        <Button>Click me</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
